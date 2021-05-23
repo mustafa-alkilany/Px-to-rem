abb014aa200cc1c74f59d54d9c696c9d4b08aea6
Mustafa alkilani <63957232+mustafa-alkilany@users.noreply.github.com>
2021-05-23 11:09:05 +0300

    Add files via upload

---
+++ rem.py
@@ -0,0 +1,8 @@
+def rem():
+    
+    print("this script was made to convert px to rem ")
+    rem_input = input("Enter the number of px that you want to convert it : ")
+    rem_output = float(rem_input) / 16
+    print (f"{rem_output} rem")
+
+rem()
